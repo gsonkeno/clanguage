a59f2de31f1bb3462a9d15c99bf882e8afb1d657
ruchen <ruchen.gs@alibaba-inc.com>
2021-11-15 11:51:29 +0800

    align

---
+++ link/Classic1.c
@@ -0,0 +1,10 @@
+//
+// Created by ruchen on 2021/11/7.
+//
+
+# include "stdio.h"
+
+// 预处理命令 gcc -E Classic1.c -o Classic1.i
+int main(){
+    printf("hello world\n")
+}
---
+++ x8664/Classic1.c
@@ -0,0 +1,10 @@
+//
+// Created by ruchen on 2021/11/6.
+//
+# include "stdio.h"
+
+int main(int argc, char *argv[]){
+    printf("hello %s\n","world");
+    return 0;
+}
+
